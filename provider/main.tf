299e7a5dac9d5e01c1b9bb40c9b8b74011f94391
srikanthch1123 <119439813+srikanthch1123@users.noreply.github.com>
2023-08-03 17:48:49 +0530

    Add files via upload

---
+++ provider/main.tf
@@ -0,0 +1,18 @@
+resource "azurerm_resource_group" "storagerg" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+resource "azurerm_storage_account" "storage" {
+  name                     = var.storage_account_name
+  resource_group_name      = azurerm_resource_group.storagerg.name
+  location                 = azurerm_resource_group.storagerg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_storage_container" "container" {
+  name                  = var.container_name
+  storage_account_name  = azurerm_storage_account.storage.name
+  container_access_type = "private"
+}
---
+++ provider/variables.tf
@@ -0,0 +1,15 @@
+variable "location" {
+  default = "eastus"
+}
+
+variable "resource_group_name" {
+  default = "storagerg"
+}
+
+variable "storage_account_name" {
+  default = "storageblobdemo001"
+}
+
+variable "container_name" {
+  default = "container001"
+}
